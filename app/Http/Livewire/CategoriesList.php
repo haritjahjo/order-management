13ed18d22b444c942495a9d43fd9277f67f11293
Hari Tjahjo <haritjahjo@gmail.com>
2023-03-26 16:54:10 +0700

    Add form modal add category

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -4,18 +4,51 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\WithPagination;
 
 class CategoriesList extends Component
 {
     use WithPagination;
-    
+    public Category $category;
+
+    public bool $showModal = false;
+
     public function render()
     {
         $categories = Category::paginate(10);
-        
+
         return view('livewire.categories-list', [
-            'categories' => $categories, 
+            'categories' => $categories,
         ]);
+    }
+
+    public function openModal()
+    {
+        $this->showModal = true;
+
+        $this->category = new Category();
+    }
+
+    public function updatedCategoryName()
+    {
+        $this->category->slug = Str::slug($this->category->name);
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+            'category.slug' => ['nullable', 'string'],
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->category->save();
+
+        $this->reset('showModal');
     }
 }
