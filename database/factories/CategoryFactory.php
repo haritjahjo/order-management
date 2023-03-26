c938cd8249cd5051590b724852cc34a0edeb1eb8
Hari Tjahjo <haritjahjo@gmail.com>
2023-03-26 16:31:40 +0700

    Add Categories livewire component

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $country = fake()->unique()->country();
+        
+        return [
+            'name' => $country,
+            'slug' => Str::slug($country),
+        ];
+    }
+}
