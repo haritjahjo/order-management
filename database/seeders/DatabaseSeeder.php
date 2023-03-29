b29af50663b8bb225fabcbaf706f6918c03b4be2
Hari Tjahjo <haritjahjo@gmail.com>
2023-03-29 10:29:52 +0700

    Add products  list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        //User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        //Category::factory(11)->create();
+
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            CountriesSeeder::class,
+            ProductSeeder::class,            
         ]);
-
-        Category::factory(11)->create();
     }
 }
